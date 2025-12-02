002f2fe3adc9b163aee7a29a169c690a120d52f5
FIRMINOenzo <enzofirmino.camp@gmail.com>
2025-12-01 22:13:15 -0300

    docs: document decision

--- src/application/use-cases/calculate-capital-gains.use-case.ts
+++ src/application/use-cases/calculate-capital-gains.use-case.ts
@@ -14,6 +14,14 @@ export class CalculateCapitalGainsUseCase {
     for (const transaction of transactions) {
       const operation = transaction.getMarketOperation();
 
+      /*
+      Usando `tax.toFixed(1)` para seguir o padrão da documentação
+      de retornar o valor com uma casa decimal. Em JavaScript, o `toFixed` retorna uma string.
+
+      Talvez seria interessante ocultar esse .0, para retonar o valor em número mesmo.
+      Ou até mesmo usar sempre em centavos, para não ter que lidar com casas decimais.
+      */
+
       if (operation === MarketOperationEnum.BUY) {
         const { tax } = portfolio.buy(transaction);
         results.push({ tax: tax.toFixed(1) });
